e347323c1918b68b6e2b30fa306f95ace330af83
Nikolea <gladchii.n@gmail.com>
2018-12-05 09:44:52 +0200

    test

---
+++ Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task15.task1519;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/* 
+Разные методы для разных типов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напиште тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+        while (!value.equals("exit")){
+            if (value.contains("."))
+                print(Double.valueOf(value));
+
+            value = scanner.nextLine();
+        }
+    }
+
+    public static void print(Double value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
+
+    public static void print(String value) {
+        System.out.println("Это тип String, значение " + value);
+    }
+
+    public static void print(short value) {
+        System.out.println("Это тип short, значение " + value);
+    }
+
+    public static void print(Integer value) {
+        System.out.println("Это тип Integer, значение " + value);
+    }
+}
